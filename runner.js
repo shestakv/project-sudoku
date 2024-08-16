f2cdc78a149b448461af69d3b93977bc2116139b
Anna Shifrina <shifrina19anna@gmail.com>
2024-08-16 16:03:22 +0300

    feat: created runner

--- runner.js
+++ runner.js
@@ -1,2 +1,12 @@
-// Используйте для решения судоку необходимые функции из файла sudoku.js
-console.log("Hellow");
+const { read } = require('./funcRead')
+const { solvedSudoku } = require('./funcSolvedSudoku')
+
+const run = () => {
+    const sudoku = read()
+    console.log(`Головоломка № ${process.argv[2]}:`);
+    console.table(sudoku);
+    console.log(`Решение головоломки № ${process.argv[2]}:`);
+    console.table(solvedSudoku(sudoku));
+    return 
+}
+run()
